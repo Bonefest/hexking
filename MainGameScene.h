cff643f671232bfdc673cd3a318bcf78ae522f27
Bonefest <tereshenko1@ukr.net>
2020-04-21 22:54:59 +0300

    Simple hex grid drawing test.

--- MainGameScene.h
+++ MainGameScene.h
@@ -5,11 +5,20 @@
 #include <vector>
 
 float radians(float degrees) {
-    cocos2d::log("%f", degrees);
     return M_PI / 180.0f * degrees;
 }
 
-std::vector<cocos2d::Vec2> hexagon(cocos2d::Vec3 position, float size) {
+cocos2d::Vec2 cubeToRect(cocos2d::Vec2 position, float size) {
+    cocos2d::Vec2 result(cocos2d::Vec2::ZERO);
+    result.x += std::round(position.x) * 1.5f * size;
+    result.y += std::round(position.x) * 0.5f * std::sqrt(3) * size;
+
+    result.y += std::round(position.y) * std::sqrt(3) * size;
+
+    return result;
+}
+
+std::vector<cocos2d::Vec2> hexagon(cocos2d::Vec2 position, float size) {
     std::vector<cocos2d::Vec2> vertices;
     for(int i = 0;i < 6; ++i) {
         vertices.push_back(cocos2d::Vec2(std::cos(radians(i * 60.0f)),
@@ -40,8 +49,13 @@ public:
     void update(float delta) {
         m_ptempHexDrawer->clear();
 
-        auto hex = hexagon(cocos2d::Vec2(200.0f, 200.0f), 24.0f);
-        m_ptempHexDrawer->drawSolidPoly(hex.data(), 6, cocos2d::Color4F::YELLOW);
+        for(int x = -2; x <= 2; x++) {
+            for(int y = -2;y <= 2; y++) {
+                auto hex = hexagon(cubeToRect(cocos2d::Vec2(x + 3, y + 1), 24.0f), 24.0f);
+                //m_ptempHexDrawer->drawSolidPoly(hex.data(), 6, cocos2d::Color4F::YELLOW);
+                m_ptempHexDrawer->drawPoly(hex.data(), 6, true, cocos2d::Color4F::WHITE);
+            }
+        }
     }
 
 private:
